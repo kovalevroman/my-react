5c5f98405da534ce1b74eca64378c941e002de47
kovalev <smolensk123>
2019-01-25 14:05:23 +0300

    react-select

--- src/App.js
+++ src/App.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import ArticleList from "./components/article-list";
 import articles from './fixtures'
 import ArticlesChart from './components/articles-chart'
+import Select from 'react-select'
 
 class App extends Component {
+  state = {
+    openItem: null
+  }
+
   render() {
     return (
       <div>
+        <Select options={this.options} value={this.state.openItem} onChange={this.handleSelect}/>
         <ArticleList articles={articles} ref={this.setArticleListRef}/>
         <ArticlesChart articles={articles}/>
       </div>
     );
   }
 
-  setArticleListRef = (ref) => {
+  get options(){
+    return articles.map(article => ({
+      label: article.title,
+      value: article.id
+    }))
+  }
+
+  handleSelect = openItem => this.setState({openItem})
+
+    setArticleListRef = (ref) => {
     console.log('---', ref);
   }
 
